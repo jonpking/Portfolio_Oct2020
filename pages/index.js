f4e2a902d983f2a9a6a0928ffac7a5e33e0fe7b4
Jonathan King <jonpking@gmail.com>
2020-10-21 18:50:05 -0700

    revamped entire site layout

--- pages/index.js
+++ pages/index.js
@@ -9,40 +9,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.headerContainer}>
-        <div className={styles.brandContainer}>
-          <img
-            className={styles.brandImage}
-            src="/images/jklogo.png"
-            alt="Jonathan King personal brand logo">
-          </img>
-        </div>
-        <div className={styles.titleContainer}>
-          <h1 className={styles.siteTitle}>Jonathan King</h1>
-          <h2 className={styles.siteSubtitle}>Full Stack Web Developer</h2>
-        </div>
-        <div className={styles.linksContainer}>
-          <img
-            src="/images/github.png"
-            alt="GitHub Mark">
-          </img>
-          <img
-            src="/images/linkedin.png"
-            alt="LinkedIn Mark">
-          </img>
-        </div>
-      </div>
+      <div className={styles.pageWrapper}>
+        <div className={styles.navContainer}>
 
-      <div className={styles.bodyContainer}>
-        <section className={styles.sections}>
-          <p>About</p>
-        </section>
-        <section className={styles.sections}>
-          <p>Project</p>
-        </section>
-        <section className={styles.sections}>
-          <p>Contact</p>
-        </section>
+        </div>
+
+        <div className={styles.bodyContainer}>
+          <section className={styles.sections}>
+            <p>About</p>
+          </section>
+          <section className={styles.sections}>
+            <p>Project</p>
+          </section>
+          <section className={styles.sections}>
+            <p>Contact</p>
+          </section>
+        </div>
       </div>
 
     </div>
