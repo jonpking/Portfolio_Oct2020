b5e8e2ad420cf3be6d41120fefd78297857b3864
Jonathan King <jonpking@gmail.com>
2020-10-26 22:38:33 -0700

    added content and minor css to about section

--- components/about.js
+++ components/about.js
@@ -4,6 +4,14 @@ export default function About() {
     return (
         <div id="about" className={styles.about}>
             About
+            <h1>Jonathan King</h1>
+            <h2>Full-Stack Web Developer</h2>
+            <p>Web developer with a passion for user-friendly UI/UX design.
+            Currently looking for new development and employment opportunities.
+            Skills include HTML, CSS, JS, Git, Bootstrap, Materialize, MySQL,
+            MongoDB, NodeJS, ExpressJS, JQuery, ReactJS, Firebase, Rest API’s,
+            AJAX.</p>
+            <img src="/images/self.jpg" className={styles.selfImage} />
         </div>
     )
 }
