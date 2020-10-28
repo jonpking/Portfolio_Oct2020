f1da0866ffac450ea58dd6b29bf0ea459dba7504
Jonathan King <jonpking@gmail.com>
2020-10-28 15:36:53 -0700

    changed content and css for about section

--- components/about.js
+++ components/about.js
@@ -3,15 +3,23 @@ import styles from "../styles/About.module.css"
 export default function About() {
     return (
         <div id="about" className={styles.about}>
-            About
-            <h1>Jonathan King</h1>
-            <h2>Full-Stack Web Developer</h2>
-            <p>Web developer with a passion for user-friendly UI/UX design.
-            Currently looking for new development and employment opportunities.
-            Skills include HTML, CSS, JS, Git, Bootstrap, Materialize, MySQL,
-            MongoDB, NodeJS, ExpressJS, JQuery, ReactJS, Firebase, Rest API’s,
-            AJAX.</p>
-            <img src="/images/self.jpg" className={styles.selfImage} />
+            <div className={styles.aboutCard}>
+                <div className={styles.aboutContent}>
+                    <p>
+                        Web developer with a passion for user-friendly UI/UX
+                        design. Currently looking for new development and
+                        employment opportunities. Skills include HTML, CSS,
+                        JS, Git, Bootstrap, Materialize, MySQL, MongoDB,
+                        NodeJS, ExpressJS, JQuery, ReactJS, Firebase, Rest
+                        API’s, AJAX.
+                    </p>
+                    <img src="/images/self.jpg" className={styles.selfImage} />
+                </div>
+                <div className={styles.aboutLinks}>
+                    <a href="#projects" className={styles.link}>View My Work</a>
+                    <a href="#contact" className={styles.link}>Contact Me</a>
+                </div>
+            </div>
         </div>
     )
 }
