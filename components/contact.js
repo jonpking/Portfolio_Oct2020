63fc660784a6a7a39d3583556b26b09857f625b7
Jonathan King <jonpking@gmail.com>
2020-10-28 20:00:47 -0700

    Revert "deleted contact"
    This reverts commit 0d3b7f1dacdbb8a942142dd4d8fc9baa83eb13e9.

---
+++ components/contact.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Contact.module.css"
+
+export default function Contact() {
+    return (
+        <div id="contact" className={styles.contact}>
+            Contact
+        </div>
+    )
+}
