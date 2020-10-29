0d3b7f1dacdbb8a942142dd4d8fc9baa83eb13e9
Jonathan King <jonpking@gmail.com>
2020-10-28 19:10:05 -0700

    deleted contact

--- components/contact.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Contact.module.css"
-
-export default function Contact() {
-    return (
-        <div id="contact" className={styles.contact}>
-            Contact
-        </div>
-    )
-}
