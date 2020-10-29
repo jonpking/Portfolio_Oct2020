4fd243d97170ff6ddc32e1c502c5e3be50c86f55
Jonathan King <jonpking@gmail.com>
2020-10-28 19:08:36 -0700

    css changes and added custom font

--- components/about.js
+++ components/about.js
@@ -6,12 +6,12 @@ export default function About() {
             <div className={styles.aboutCard}>
                 <div className={styles.aboutContent}>
                     <p>
-                        Web developer with a passion for user-friendly UI/UX
-                        design. Currently looking for new development and
-                        employment opportunities. Skills include HTML, CSS,
-                        JS, Git, Bootstrap, Materialize, MySQL, MongoDB,
-                        NodeJS, ExpressJS, JQuery, ReactJS, Firebase, Rest
-                        API’s, AJAX.
+                        Full-stack web developer with a passion for
+                        user-friendly UI/UX design. Currently looking
+                        for new development and employment opportunities.
+                        Skills include HTML, CSS, JS, Git, Bootstrap,
+                        Materialize, MySQL, MongoDB, NodeJS, ExpressJS,
+                        JQuery, ReactJS, Firebase, Rest API’s, AJAX.
                     </p>
                     <img src="/images/self.jpg" className={styles.selfImage} />
                 </div>
