0be289085ea2deb10f67adf1d2053b4d6c7c5673
Jonathan King <jonpking@gmail.com>
2020-11-02 19:55:59 -0700

    added temp content to projects component and started styling

--- components/projects.js
+++ components/projects.js
@@ -1,9 +1,26 @@
 import styles from "../styles/Projects.module.css"
+import Link from "next/link"
 
 export default function Projects() {
     return (
         <div id="projects" className={styles.projects}>
-            Projects
+            <div className={styles.projectsCard}>
+                <h2>Project Name</h2>
+                <p>
+                    Project Description Project Description Project Description
+                    Project Description Project Description Project Description
+                    Project Description Project Description Project Description
+                </p>
+                <img src="/images/self.jpg"></img>
+                <div>
+                    <Link href="#">
+                        <a>Repo</a>
+                    </Link>
+                    <Link href="#">
+                        <a>Live</a>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
