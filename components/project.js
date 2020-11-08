b5f39cbfb8ecd4863c357c42c49fc7c4b36de195
Jonathan King <jonpking@gmail.com>
2020-11-07 19:46:39 -0700

    adjusted temp text and reorganized project component

--- components/project.js
+++ components/project.js
@@ -5,12 +5,6 @@ export default function Project() {
     return (
         <div className={styles.projectCard}>
             <h2>Project Name</h2>
-            <p>
-                Project Description Project Description Project Description
-                Project Description Project Description Project Description
-                Project Description Project Description Project Description
-                </p>
-            <img src="/images/self.jpg"></img>
             <div>
                 <Link href="#">
                     <a>Repo</a>
@@ -19,6 +13,19 @@ export default function Project() {
                     <a>Live</a>
                 </Link>
             </div>
+            <p>
+                An app that pulls various gifs from the giphy API based on
+                keywords that can be submitted by the user
+                <br />
+                <br />
+                By clicking the buttons on the screen you will be provided
+                with 10 gifs from the giphy API. You can add more search
+                buttons by typing in the input box and pressing the "Add a
+                new comic!" button. Each gif will also have a rating posted
+                to the screen with it in a manner similar to movie ratings
+                (ie "g", "pg", "r", etc.).
+            </p>
+            <img src="/images/self.jpg"></img>
         </div>
     )
 }
